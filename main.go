f586ca78c9af21293a8089bf58028bffa6148f5f
n-yata <yata1214@gmail.com>
2024-09-13 06:26:40 +0900

    HTTPリクエストの追加

--- main.go
+++ main.go
@@ -2,24 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age  int    `json:"How old are you?"`
+type FuncRequest struct {
+	URL     string            `json:"url,omitempty"`
+	Method  string            `json:"method,omitempty"`
+	Headers map[string]string `json:"headers,omitempty"`
+	Body    string            `json:"body,omitempty"`
 }
 
-type MyResponse struct {
-	Message string `json:"Answer"`
+type FuncResponse struct {
+	Status string `json:"status"`
+	Body   string `json:"text"`
 }
 
-func HandleLambdaEvent(event *MyEvent) (*MyResponse, error) {
+func HandleLambdaEvent(event *FuncRequest) (*FuncResponse, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
-	return &MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+
+	var reqBody io.Reader
+	if event.Body != "" {
+		reqBody = strings.NewReader(event.Body)
+	}
+
+	req, _ := http.NewRequest(event.Method, event.URL, reqBody)
+	for key, value := range event.Headers {
+		req.Header.Set(key, value)
+	}
+
+	client := new(http.Client)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %v", err)
+	}
+	defer res.Body.Close()
+
+	byteArray, _ := io.ReadAll(res.Body)
+	return &FuncResponse{Status: res.Status, Body: string(byteArray)}, nil
 }
 
 func main() {
